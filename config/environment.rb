364e81e2cf70e13a40299f953bd2dc8c7fd367bb
jdegraad <jdegraad@gail.com>
2014-11-25 03:25:09 +0000

    Sinatra specific code

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require 'pry' if development?
+#require 'pry' if Rails.development?
 
 # Initialize the Rails application.
 Rails.application.initialize!
