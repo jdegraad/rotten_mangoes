1a037f5ee73eb226cb6b75dfdbb694a008c0b6db
jdegraad <jdegraad@gail.com>
2014-11-25 03:04:01 +0000

    second commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
+  has_many :reviews
   validates :title, presence: true
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
@@ -6,6 +7,10 @@ class Movie < ActiveRecord::Base
   validates :poster_image_url, presence: true
   validates :release_date, presence: true
   validate :release_date_is_in_the_future
+
+  def review_average
+    reviews.sum(:rating_out_of_ten)/reviews.size
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  resources :movies
 
-  get 'movies/index'
-
-  get 'movies/show'
-
-  get 'movies/new'
-
-  get 'movies/edit'
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
