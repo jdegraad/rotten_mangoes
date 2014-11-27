ad0727f958eeef675c1b18755eaf84a1bb9fea9b
jdegraad <jdegraad@gail.com>
2014-11-27 19:29:37 +0000

    admin stuff

--- Gemfile
+++ Gemfile
@@ -46,4 +46,5 @@ group :production do
 end
 
 gem "rmagick"
+gem "kaminari"
 
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_filter :is_admin?
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(10)
   end
 
   def make_admin
@@ -11,6 +11,44 @@ class Admin::UsersController < ApplicationController
     @user.admin = true
     @user.save
     redirect_to admin_users_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      
+      redirect_to admin_users_path, notice: "Welcome aboard, #{@user.firstname}!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+   protected
+
+  def user_params
+    params.require(:user).permit(
+      :firstname, :lastname, :email, :admin, :password, :password_confirmation
+       
+    )
   end
 
 end
